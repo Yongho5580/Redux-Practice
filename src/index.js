098a32c2eda6b7e8a172511611d113cf909898b2
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-06-18 20:40:54 +0900

    Start Redux Todo-List

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return;
+    default:
+      return state; 
+  }
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    li.id =  toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+  })
+}
+
+store.subscribe(paintToDos);
+
+const addToDo = text => {
+  store.dispatch({ type: ADD_TODO, text })
+}
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  addToDo(toDo)
+}
+
+form.addEventListener("submit", onSubmit);
+
