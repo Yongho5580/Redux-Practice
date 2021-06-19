6b5fde553ce4ba601f5fd84da3ff09ead671d2d6
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-06-19 19:54:27 +0900

    ToDoList finished

--- src/index.js
+++ src/index.js
@@ -1,50 +1,7 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+import { Provider } from "react-redux";
+import store from "./store";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
-    case DELETE_TODO:
-      return;
-    default:
-      return state; 
-  }
-}
-
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()));
-
-const paintToDos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = "";
-  toDos.forEach(toDo => {
-    const li = document.createElement("li");
-    li.id =  toDo.id;
-    li.innerText = toDo.text;
-    ul.appendChild(li);
-  })
-}
-
-store.subscribe(paintToDos);
-
-const addToDo = text => {
-  store.dispatch({ type: ADD_TODO, text })
-}
-
-const onSubmit = e => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  addToDo(toDo)
-}
-
-form.addEventListener("submit", onSubmit);
-
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
