83126aeea344078090708bd897fb7d09f80fbc27
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-06-19 21:19:30 +0900

    Today's Done

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,7 @@
+import React from "react";
+
+function Detail () {
+    
+}
+
 export default () => "Detail";
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,29 +1,39 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import ToDo from "../components/ToDo";
+import { actionCreators } from "../store";
 
-
-function Home () {
-    const [text, setText] = useState("");
-    
-    function onChange (e) {
-        setText(e.target.value);
-    }
-
-    function onSubmit (e) {
-        e.preventDefault();
-        console.log(text);
-    }
-
-    return (
-        <>
-        <h1>To Do</h1>
-        <form onSubmit={onSubmit}>
-            <input type="text" value={text} onChange={onChange} />
-            <button>ADD</button>
-        </form>
-        <ul></ul>
-        </>
-    );
+function Home({ toDos, addToDo }) {
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    addToDo(text);
+    setText("");
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>{toDos.map(toDo => <ToDo {...toDo} key={toDo.id} />)}</ul>
+    </>
+  );
 }
 
+function mapStateToProps(state) {
+  return { toDos: state };
+}
 
-export default Home;
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: text => dispatch(actionCreators.addToDo(text))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
+
