5c14be918e53ce93d1d3d197a7d969c2416b607b
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-06-19 21:25:31 +0900

    For Tomorrow

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-function Detail () {
-    
+function Detail ({ toDos }) {
+    const id = useParams();
+    console.log(id);
+    return <h1>Detail</h1>;
 }
 
-export default () => "Detail";
+function mapStateToProps(state, ownProps) {
+    return { toDos: state }
+}
+
+export default Detail;
